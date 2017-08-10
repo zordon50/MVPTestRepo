71faa0c93be0a2c57b0e8b750ed2dd4e757f177c
ahong211 <ahong211@gmail.com>
2017-08-10 00:29:13 -0700

    Add comment to provoke merge conflict

--- app/src/main/java/com/albert/mvp/DataRepository.java
+++ app/src/main/java/com/albert/mvp/DataRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class DataRepository {
     public List<String> getFamilyNames() {
         // Retrieve dummy data
+        // Add this comment to hopefully provoke a merge conflict
         return Arrays.asList("Albert", "Sally", "Ollie");
     }
 }
